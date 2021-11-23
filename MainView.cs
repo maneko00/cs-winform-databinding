5424b643cf89ec02a25e06454cda917c62d54f84
maneko00 <82707685+maneko00@users.noreply.github.com>
2021-11-23 22:51:57 +0900

    Add files via upload

---
+++ MainView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+using WindowsMvvm.ViewModels;
+
+namespace WindowsMvvm.Views
+{
+    public partial class MainView : Form
+    {
+        private MainViewModel view_model_ = new MainViewModel()
+;       public MainView()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+
+            this.MyTextBox.DataBindings.Add("Text", this.view_model_, "MyTextBoxText");
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //button1.Text = MyTextBox.Text;
+            this.view_model_.Call();
+        }
+    }
+}
---
+++ MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsMvvm.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private string my_textbox_text_ = string.Empty;
+        public string MyTextBoxText
+        {
+            get { return my_textbox_text_; }
+            set
+            {
+                SetProperty(ref my_textbox_text_, value, "MyTextBoxText");
+            }
+        }
+
+        public void Call()
+        {
+            MyTextBoxText = "BBb";
+        }
+    }
+}
+
+//namespace WindowsMvvm.ViewModels
+//{
+//    public class MainViewModel
+//    {
+//        private string my_textbox_text_ = string.Empty;
+//        public string MyTextBoxText
+//        {
+//            get { return my_textbox_text_; }
+//            set
+//            {
+//            }
+//        }
+
+//        public void Call()
+//        {
+//            MyTextBoxText = "BBb";
+//        }
+//    }
+//}
